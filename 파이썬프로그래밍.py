f5d9b6e4a0160a358c51dece0b88cc59af0e71ef
somgam6373 <somgam6373@gmail.com>
2022-05-24 16:09:26 +0900

    name

--- 파이썬프로그래밍.py
+++ 파이썬프로그래밍.py
@@ -1,7 +1,16 @@
-favor = []
+# 동물명 영어사전
+'''
+dic = {"강아지":"dog", "고양이":"cat", "새":"bird", "코끼리":"elephant"}
+
 while True:
-    fruit = input(":")
-    if fruit == str(0):
+    word = input("단어를 입력하시오(종료:0):")
+    
+    if word == '0':
+        print("종료되었습니다.")
         break
-    favor.append(fruit)
-print(favor)
+    elif word in dic:
+        print(word," : ",dic[word])
+    elif word not in dic:
+        print("사전에 없는 단어입니다.")
+    
+'''
