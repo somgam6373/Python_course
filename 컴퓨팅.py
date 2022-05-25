0b3f07a97501ba3f58e5559f44a6e968401ab74f
somgam6373 <somgam6373@gmail.com>
2022-05-25 14:56:40 +0900

    컴퓨팅 사고와 데이터의 이해

--- 컴퓨팅.py
+++ 컴퓨팅.py
@@ -10,10 +10,52 @@
 
 
 
-scores = [50, 30, 90, 70, 60]
-print("현재 입력된 점수는\n",scores,"이고, 모두 5개 입니다.")
+score = [50, 30, 90, 70, 60]
+idx, val, cnt = 0, 0, 0
 
+print("현재 입력된 점수는")
+print(score, "이고, 모두", len(score), '개 입니다.')
+
+print()
 print("append 연습")
-print("append 할 값: 100")
-scores.append(100)
-print(scores)
+val = int(input('append 할 값 : '))
+score.append(val)
+print(score)
+
+print()
+print("insert 연습")
+idx = int(input("insert할 위치 : "))
+val = int(input("insert 할 값 : "))
+score.append(val)
+print(score)
+
+print()
+print("index 연습")
+val = int(input("찾고 싶은 값 : "))
+idx = score.index(val)
+print(score)
+print("%d는 %d번째 위치하고 있습니다." % (val, idx))
+
+print()
+print("remove 연습")
+val = int(input("삭제하고 싶은 값 : "))
+score.remove(val)
+print(score)
+
+print()
+print("del 연습")
+idx = int(input("삭제할 위치 : "))
+del(score[idx])
+print(score)
+
+print()
+print("count 연습")
+val = int(input("갯수를 알고 싶은 값 : "))
+cnt = score.count(val)
+print(score)
+print("%d는 %d번 나옵니다." %(val, cnt))
+
+print()
+print("reverse sort 연습")
+score.reverse()
+print(score)
