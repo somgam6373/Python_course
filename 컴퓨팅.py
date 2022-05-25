1e2d0926310fe3d880147bf781f4c1448808588b
somgam6373 <somgam6373@gmail.com>
2022-05-25 13:21:27 +0900

    컴퓨팅사고와데이터의 이해

--- 컴퓨팅.py
+++ 컴퓨팅.py
@@ -1,1 +1,19 @@
-print('{0:d} {1:5d} {2:05d}'.format(1,2,3))
+#리스트 추가: append
+#리스트 삭제: remove
+#항목이 리스트 안에 있는지 체크: if ~ in ~
+#리스트 삭제: del ~
+#리스트 마지막 항목: pop
+#리스트 탐색하기: index(순서가 나타남)
+#리스트 정렬하기: sort
+#해당되는 값의 개수: count
+#리스트의 길이: len
+
+
+
+scores = [50, 30, 90, 70, 60]
+print("현재 입력된 점수는\n",scores,"이고, 모두 5개 입니다.")
+
+print("append 연습")
+print("append 할 값: 100")
+scores.append(100)
+print(scores)
