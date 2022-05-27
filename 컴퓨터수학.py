766de95f2f4a0ac2b9c9bacb6d95cbd428ea3fa4
somgam6373 <somgam6373@gmail.com>
2022-05-28 00:17:26 +0900

    vector

--- 컴퓨터수학.py
+++ 컴퓨터수학.py
@@ -1,7 +1,20 @@
+#np.dot : 내적
+#np.cross : 외적
+
+import numpy as np
+a = np.array([1,-1,1])
+b = np.array([1,3,6])
+c = np.array([2,-5,3])
+
+first = a+b
+first_1 = np.dot(first,c)
+print("(a+b)c =",first_1)
+
+second = np.dot(a,c)+np.dot(b,c)
+print("ac+bc =",second)
+
 import sympy as sp
-x = sp.Symbol('x')
-fx = x+1/x-1/x**2
-gx = (sp.sqrt(x) + 4)/x
-print(sp.integrate(fx,x))
-print(sp.integrate(gx,x))
+k = sp.Symbol("k")
+third = k*np.dot(a,b)
+print(third)
 
