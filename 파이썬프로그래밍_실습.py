04e7416bf80e7afedd3bdd2f11062ef09690d1bd
somgam6373 <somgam6373@gmail.com>
2022-05-31 16:11:56 +0900

    computer_math

--- 파이썬프로그래밍_실습.py
+++ 파이썬프로그래밍_실습.py
@@ -10,12 +10,16 @@
 n = int(input("정수를 입력하시오."))
 print(n, "!=", factorial(n))
 '''
-def fib( n):
-    if(n==0):
-        return 0
-    else if(n==1):
-        return 1
+'''
+def hanoi_tower(n, org, tmp, to):
+    if(n==1):
+        print("원판 1을",org,"에서",to,"로 옮긴다.")
     else:
-        return fib(n-1) + fib(n-2)
+        hanoi_tower(n-1,org,to,tmp)
+        print("원판", n,"을", org,"에서", to, "로 옮긴다.")
+        hanoi_tower(n-1,tmp,org,to)
+hanoi_tower(4,"A","B","C")
+'''
 
-
+import numpy as np
+import matplotlib.pyplot as plt
