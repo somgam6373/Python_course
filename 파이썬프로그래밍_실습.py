cec7481aa1c5293d88560caff4e31942c5225833
somgam6373 <somgam6373@gmail.com>
2022-06-08 12:26:38 +0900

    python_png

--- 파이썬프로그래밍_실습.py
+++ 파이썬프로그래밍_실습.py
@@ -1,25 +1,38 @@
-#except: 예외상황 발생 시 실행되는 명령
-#finally: 예외상황이 발생했든 안 했든 마지막 문장 실행
-
-'''def factorial(n):
-    if n == 1:
-        return(1)
-    else:
-        return n*factorial(n-1)
-
-n = int(input("정수를 입력하시오."))
-print(n, "!=", factorial(n))
-'''
-'''
-def hanoi_tower(n, org, tmp, to):
-    if(n==1):
-        print("원판 1을",org,"에서",to,"로 옮긴다.")
-    else:
-        hanoi_tower(n-1,org,to,tmp)
-        print("원판", n,"을", org,"에서", to, "로 옮긴다.")
-        hanoi_tower(n-1,tmp,org,to)
-hanoi_tower(4,"A","B","C")
-'''
+#plt.title("이준혁")
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+data_file = np.genfromtxt("c:\\pythontest\\data_file.txt",delimiter=',')
+print(data_file.shape)
+print(data_file[0:2,:])
+
+time = data_file[:,0]
+time = time - time[0]
+
+sensors = data_file[:,1:5]
+
+print(sensors.shape)
+print(sensors[0:3])
+
+avg = np.mean(sensors,1) # over the 2nd dimension
+
+print(avg[0:3])
+
+my_data = np.hstack((time.reshape(1200,1),
+                    sensors,
+                    avg.reshape(1200,1)))
+
+np.savetxt('c:\\pythontest\\data.txt',(1200 , 9),delimiter=',')
+
+plt.plot(time/60.0, sensors[:, 1], 'ro', label = "Sensor 2")
+plt.plot(time/60.0, sensors[:, 2], 'gx', label = "Sensor 3")
+plt.plot(time/60.0, avg, 'b.', label = "Average Sensors 1-4")
+
+plt.legend()
+plt.xlabel('Time (min)')
+plt.ylabel('Sensor Values')
+plt.savefig('c:\\pythontest\\202210962_이준혁.png')
+plt.title("202210962_LeeJunHyuk")
+plt.show()
+
