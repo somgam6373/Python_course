0ab0d9137dc072439a35abc443291dbfc261cbcb
somgam6373 <somgam6373@gmail.com>
2022-06-10 13:38:06 +0900

    컴퓨팅

--- 컴퓨터수학_실습.py
+++ 컴퓨터수학_실습.py
@@ -1,11 +1,9 @@
 import numpy as np
-p = np.array([1,-1,1])
-q = np.array([1,3,6])
-r = np.array([2,-5,3])
-
-pq = q - p
-pr = r - p
-
-print(pq)
-print(pr)
-print(np.cross(pq,pr))
+A = np.zeros((3,2))
+sum = 0
+import numpy as np
+for i in range(1,4):
+    for j in range(1,3):
+        A[i-1][j-1] = i+2*j
+        sum = sum + A[i-1][j-1]
+        print(A)
