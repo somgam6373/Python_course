632c10d9768990b294cdf6b8d9d506c6c5fe5f78
somgam6373 <somgam6373@gmail.com>
2022-06-14 16:20:22 +0900

    python

--- 컴퓨팅.py
+++ 컴퓨팅.py
@@ -13,21 +13,3 @@
 #큐: 가장 최근에 입력된 것부터 출력
 #isalpha: 문자열이 알파벳인지 아닌지 판별함
 #'while qu:' 의 이유 : qu의 리스트가 없어질 때까지 반복
-
-player = {"name":"anemone",
-        "gender":"male",
-        "job":"magician",
-        "level":"999",}
-
-
-key = input("접근하고자 하는 키:")
-
-if key in player:
-    print(player[key])
-
-elif key not in player:
-    print("잘못입력했습니다.")
-'''value = player.get("존재하지 않는 키")
-print("value")
-if value == None:
-    print("존재하지 않는 키에 접근했습니다.")'''
--- 파이썬프로그래밍_실습.py
+++ 파이썬프로그래밍_실습.py
@@ -1,3 +1,4 @@
+'''
 #plt.title("이준혁")
 
 import numpy as np
@@ -33,4 +34,72 @@ plt.ylabel('Sensor Values')
 plt.savefig('c:\\pythontest\\202210962_이준혁.pdf')
 plt.title("202210962_LeeJunHyuk")
 plt.show()
+'''
 
+# Pandas => 데이터를 테이블 형태로 다룬다.
+# DataFrame: 데이터 테이블 전체 객체
+# Series: 각 열 데이터를 다루는 개체
+
+# 한글이 있으면 한글과 관련된 인코딩을 해야만함
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+data_file = pd.read_csv('c:\\pythontest\\data_with_headers.csv')
+
+time = data_file['time']
+time = time - time[0]
+
+sensors = data_file.loc[:,'s1': 's4']
+avg_row = np.mean(sensors,1)
+avg_col = np.mean(sensors, 0)
+
+result = pd.concat([time, sensors, avg_row], axis =1)
+result.to_csv('c:\\pythontest\\result.csv')
+#result.to_excel('c:\\pythontest\\result.xlsx')
+result.to_html('c:\\pythontest\\result.htm')
+
+plt.plot(time, sensors['s1'], 'r-', label = 'Sensor 2')
+plt.plot(time, avg_row, 'b', label = 'Average')
+
+plt.xlabel('Time (sec)')
+plt.ylabel('Sensor Values')
+plt.legend()
+plt.title('202210962_LeeJunHyuk')
+plt.savefig('c:\\pythontest\\my_Python_plot.png')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''흰_눈_아래
+환희_웃던
+그대_모습_보고싶어라
+
+따뜻한_눈꽃에
+향기만이_가득하네
+
+사계절_맴돈
+짙은_암향
+매화_아닌_제비였으니
+
+이별과_작별_사이
+그_무언가
+슬픔이여_안녕
+슬픔이여_안녕'''
