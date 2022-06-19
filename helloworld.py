9c565b90d9178b263e76fe25ebf17ee32e12c573
somgam6373 <somgam6373@gmail.com>
2022-06-19 17:01:07 +0900

    jin

--- helloworld.py
+++ helloworld.py
@@ -13,7 +13,7 @@ print("%d의 %d승은"%(a,b),exp(a,b))
 '''
 
 #2(3분반)
-
+'''
 def f(n):
     if n < 0:
         return '0'
@@ -25,3 +25,22 @@ def f(n):
         return f(int(n/2)) #+ '1'
 n = int(input(":"))
 print(f(n))
+'''
+
+#3(3분반)
+
+def f(a,b):
+    if a == 2:
+        return '3 5 7 11 13'
+    for i in range(2,a):
+        if a%i != 0:
+            return 
+        else:
+
+
+    
+
+    
+a = int(input("a 입력:"))
+b = int(input("b 입력:"))
+print("%d보다 큰 %d개의 가장 작은 소수:"%(a,b),f(a,b))
--- 컴퓨터수학.py
+++ 컴퓨터수학.py
@@ -1,6 +1,28 @@
-x = int(input("십진수 숫자 입력:"))
-y = ''
-while x > 0:
-    y = str(x%2) + y
-    x//=2
-print(y)
+a = int(input("검사하려는 수를 입력하시오:"))
+
+def IS_PNum(a):
+    if(len(str(a)) == 1):
+        if((a == 2) or (a == 3) or (a == 5) or (a ==7)):
+            return("소수입니다.")
+        else:
+            return("소수가 아닙니다.")
+    if(len(str(a)) >= 2):
+        if((a % 2 == 0) or (a % 3 == 0) or (a % 5 == 0) or (a % 7 == 0)):
+            return("소수가 아닙니다.")
+        else:
+            return("소수입니다.")
+print(IS_PNum(a))
+b = int(input("수를 입력:"))
+
+def Print_PNum (a , b): 
+    list = []
+    while(True):  
+        result = IS_PNum(a)  
+        if(result == '소수입니다.'):
+            list.append(a)
+        if(len(list) > b):
+            break
+        a += 1
+    return(list)
+
+print(Print_PNum(a , b))
