ec679e144c6941393bf6cb63f92c691cf063df4c
somgam6373 <somgam6373@gmail.com>
2022-10-05 13:10:57 +0900

    5주차

--- 알고리즘과게임컨텐츠.py
+++ 알고리즘과게임컨텐츠.py
@@ -1,42 +1,40 @@
+#1
+'''print("삼각형의 세변의 길이를 변c가 가장 길게 입력해주세요.")
+a = int(input("변 a의 길이:"))
+b = int(input("변 b의 길이:"))
+c = int(input("변 c의 길이:"))
+
+if c*c == a*a + b*b:
+    print("직각 삼각형입니다.")
+else:
+    print("직각 삼각형이 아닙니다.")
 '''
+#2
 
-money, c500, c100, c50, c10 = 0, 0, 0, 0, 0
+import turtle
 
-money = int(input("교환할 돈은 얼마?:"))
+turtle.shape("turtle")
+select = int(input("그리고 싶은 도형을 선택하세요 (1.삼각형, 2. 사격형, 3.원) >>>"))
 
-c500 = money // 500
-money %= 500
+if select == 1:
+    turtle.forward(100)
+    turtle.right(120)
+    turtle.forward(100)
+    turtle.right(120)   
+    turtle.forward(100)
+    turtle.right(120)  
 
-c100 = money // 100
-money %= 100
+elif select == 2:
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(100)
+    turtle.right(90)
 
-c50 = money // 50
-money %= 50
+elif select == 3:
+    turtle.circle(50)  
 
-c10 = money // 10
-money %= 10
-
-print("\n 500원짜리 ==> %d개" % c500)
-print(" 100원짜리 ==> %d개" % c100)
-print(" 50원짜리 ==> %d개" % c50)
-print(" 10원짜리 ==> %d개" % c10)
-print(" 바꾸지 못한 잔동 ==> %d원" % money)
-
-'''
-
-sel = int(input("입력진수 결정(16/10/8/2):"))
-num = input("값 입력:")
-
-if sel == 16:
-    num10 = int(num, 16)
-if sel == 10:
-    num10 = int(num, 10)
-if sel == 8:
-    num10 = int(num, 8)
-if sel == 2:
-    num10 = int(num, 2)
-
-print("16진수 ==>", hex(num10))
-print("10진수 ==>", num10)
-print("8진수 ==>", oct(num10))
-print("2진수 ==>", bin(num10))
+turtle.done()         
